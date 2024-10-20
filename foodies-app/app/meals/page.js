c94d5e19f074691046f92ce0cff46d533f47a5c1
prateekbadjatya <prateek.badjatya@gmail.com>
2024-10-21 00:59:06 +0530

    add meals and community page

--- foodies-app/app/meals/page.js
+++ foodies-app/app/meals/page.js
@@ -1,13 +1,25 @@
-import Link from 'next/link'
-import React from 'react'
-
+import Link from "next/link";
+import React from "react";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 const Mealspage = () => {
   return (
-    <main>
-        <h1>Mealspage</h1>
-        <p><Link href='/meals/share'>Meals Share</Link></p>
-    </main>
-  )
-}
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delecious meals created{" "}
+          <span className={classes.highlight}>By you</span>
+        </h1>
+        <p>Chosse you fav recipe and cook it youself. It is easy and fun</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share"> Share Your favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
+};
 
-export default Mealspage
+export default Mealspage;
